256e1ac49587bc402ab9bb2fef74ce4a7c231e1d
Om Phule <omphule77@gmail.com>
2026-05-17 10:34:32 +0530

    Migrate MySQL to MongoDB

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,11 @@
 from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app(config_class=Config):
     application = Flask(__name__)
     application.config.from_object(config_class)
 
-    db.init_app(application)
-    migrate.init_app(application, db)
-    
-    from app import routes, models
+    from app import routes
     application.register_blueprint(routes.bp)
 
     return application
--- config.py
+++ config.py
@@ -7,28 +7,5 @@ class Config:
     # DATABASE CONFIGURATION
     # -----------------------------
 
-    # If DATABASE_URL exists (Production - AWS)
-    # use it directly
-    if os.environ.get("DATABASE_URL"):
-        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","mysql+pymysql://admin:Omphule13@billing-mysql.cd8q4eekysoe.eu-north-1.rds.amazonaws.com:3306/billing_system")
-    else:
-        # Local development fallback
-        MYSQL_HOST = os.environ.get("DB_HOST", "localhost")
-        MYSQL_USER = os.environ.get("DB_USER", "root")
-        MYSQL_PASSWORD = os.environ.get("DB_PASSWORD", "Omphule13@")
-        MYSQL_DB = os.environ.get("DB_NAME", "mysql")
-        MYSQL_PORT = os.environ.get("DB_PORT", "3306")
-
-        SQLALCHEMY_DATABASE_URI = (
-            f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
-            f"@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
-        )
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Optional: For direct PyMySQL usage
-    MYSQL_HOST = os.environ.get("DB_HOST", "localhost")
-    MYSQL_USER = os.environ.get("DB_USER", "root")
-    MYSQL_PASSWORD = os.environ.get("DB_PASSWORD", "Omphule13@")
-    MYSQL_DB = os.environ.get("DB_NAME", "billing_system")
-    MYSQL_PORT = int(os.environ.get("DB_PORT", 3306))
+    # MongoDB connection URI (use local if not specified)
+    MONGO_URI = os.environ.get("mongodb+srv://omphule77_db_user:<db_password>@cluster0.xwlfth2.mongodb.net/", "mongodb+srv://omphule77_db_user:Omphule77@cluster0.xwlfth2.mongodb.net/")
